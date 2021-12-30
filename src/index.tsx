92756c9cec712755796d70a8c8abbae25401100d
Lília Paula <liliaxtz2@gmail.com>
2021-12-30 00:24:42 -0300

    init

--- src/index.tsx
+++ src/index.tsx
@@ -3,12 +3,13 @@ import ReactDOM from 'react-dom'
 import './index.css'
 import App from './App'
 import reportWebVitals from './reportWebVitals'
-import {BrowserRouter} from 'react-router-dom'
+import {BrowserRouter, Link} from 'react-router-dom'
 
 ReactDOM.render(
   <React.StrictMode>
     <BrowserRouter>
       <App />
+      <Link to="/login" style={{color: 'pink'}}>Login</Link>
     </BrowserRouter>
   </React.StrictMode>,
   document.getElementById('root')
